b584ca1b98915c25f6098ea0757b3dbf408d3c27
remyampr <remyaradhakrishnan2992@gmail.com>
2025-03-04 18:57:19 +0400

    webhook

--- server/Routes/paymentRoutes.js
+++ server/Routes/paymentRoutes.js
@@ -11,7 +11,7 @@ router.post("/makepayment",protect,authorize(["user"]),paymentFunction);
 // Handle successful payment webhook
 // router.post("/webhook", express.raw({type: 'application/json'}), stripeWebhookHandler);
 
-router.post("/webhook", stripeWebhookHandler);
+router.post("/", stripeWebhookHandler);
 
 
 
