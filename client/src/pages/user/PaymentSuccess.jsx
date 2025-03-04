b6cdfd5e0999fe0e096573811ccda3ea923f930c
remyampr <remyaradhakrishnan2992@gmail.com>
2025-03-04 19:22:27 +0400

    toast

--- client/src/pages/user/PaymentSuccess.jsx
+++ client/src/pages/user/PaymentSuccess.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const PaymentSuccess = () => {
   const [loading, setLoading] = useState(true);
