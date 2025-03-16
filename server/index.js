54c5197672809dd2194f719677d80bd8099b4b44
remyampr <remyaradhakrishnan2992@gmail.com>
2025-03-16 18:47:59 +0400

    vercel

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(cookieparser());
 
 
 app.use(cors({
-  origin: ['https://fitness-appfrontend.vercel.app/', 'http://localhost:5174'],
+  origin: ['https://fitness-appfrontend.vercel.app', 'http://localhost:5174'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
